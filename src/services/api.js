754f50b9d610b72b9126db49cbf1b26f93108f36
FelipeDev <130187236+felipe27-dev@users.noreply.github.com>
2025-11-05 16:44:38 -0400

    Remove headers from axios API configuration
    Removed custom headers from axios instance.

--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: "https://api-go-imovel.onrender.com",
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
-
-
 
 export default api;
