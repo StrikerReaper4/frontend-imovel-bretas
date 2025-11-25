784c219c9c51afa6056b1b156b2d16238b8f9fe2
Strikerreaper4 <strikerreaper4@gmail.com>
2025-11-25 19:37:00 +0000

    apontando para nova API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api-go-imovel.onrender.com",
+  baseURL: "https://api.bretasimoveis.cloud",
 });
 
 export default api;
